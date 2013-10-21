df6378c0050a1766e0df6ec4cf62a8260784f031
Teliatko <teliatko@gmail.com>
2013-10-21 23:04:52 +0200

    Immutable fields

--- src/main/scala/at/devfest/fnddd/orders/domain/order.scala
+++ src/main/scala/at/devfest/fnddd/orders/domain/order.scala
@@ -9,60 +9,59 @@ object order {
   import at.devfest.fnddd.orders.domain.customer._
   import at.devfest.fnddd.orders.domain.product._
 
-  import scala.collection.mutable.ArrayBuffer
-
-  /** Order ID domain value */
-  class OrderId(var id: String)
+  /** Order ID domain value, implemented as value class */
+  case class OrderId(id: String) extends AnyVal
 
   /** Address domain value */
   class Address(
-    var street: String,
-    var zipCode: String,
-    var city: String,
-    var country: String)
+    val street: String,
+    val zipCode: String,
+    val city: String,
+    val country: String)
 
   /** Order line domain value */
   class OrderLine(
-    var productId: ProductId,
-    var quantity: Int,
-    var unit: ProductUnit,
-    var price: BigDecimal )
+    val productId: ProductId,
+    val quantity: Int,
+    val unit: ProductUnit,
+    val price: BigDecimal )
 
   /** Order domain entity, aggregate root */
   class Order(
-    var id: OrderId,
-    var customerId: CustomerId,
-    var shipmentAddress: Address,
-    var lines: ArrayBuffer[OrderLine] = ArrayBuffer.empty,
-    var created: DateTime = DateTime.now()) {
+    val id: OrderId,
+    val customerId: CustomerId,
+    val shipmentAddress: Address,
+    val lines: Vector[OrderLine] = Vector.empty,
+    val created: DateTime = DateTime.now()) {
 
     /** Example of aggregate root method */
     def addProduct(
       productId: ProductId, quantity: Int,
-      unit: ProductUnit, price: BigDecimal) {
+      unit: ProductUnit, price: BigDecimal): Order = {
       // Just checking some invariants
       require(quantity > 0, s"Quantity must be greater than 0")
       require(price > 0, s"Price must be greater than 0")
 
-      // Lookup if product is not already in order
+      // Lookup if product is not in order
       val (existing, rest) = lines.partition( _.productId == productId )
       require(existing.isEmpty, s"$productId not in order")
 
       // Add product to order
       val line = new OrderLine(productId, quantity, unit, price)
-      lines += line
+      new Order(this.id, this.customerId, this.shipmentAddress, this.lines :+ line, this.created)
     }
 
-    /**
-     * Another methods for manipulating order. Left unimplemented,
-     * similar approach like in addProduct.
-     */
+   /**
+    * Another methods for manipulating order. Left unimplemented,
+    * similar approach like in addProduct.
+    */
 
-    def removeProduct(productId: ProductId) { ??? }
+    def removeProduct(productId: ProductId): Order = { ??? }
 
-    def updateQuantity(productId: ProductId, quantity: Int) { ??? }
+    def updateQuantity(productId: ProductId, quantity: Int): Order = { ??? }
 
-    def changeShippingAddress(address: Address) { ??? }
+    def changeShippingAddress(address: Address): Order = { ??? }
+
 
   }
 
