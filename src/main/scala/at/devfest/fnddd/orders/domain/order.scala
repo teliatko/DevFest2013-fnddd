ec210ba8d9306da839effa877eb3a80231d69769
Teliatko <teliatko@gmail.com>
2013-10-21 22:51:31 +0200

    Basic anemic model

--- src/main/scala/at/devfest/fnddd/orders/domain/order.scala
+++ src/main/scala/at/devfest/fnddd/orders/domain/order.scala
@@ -4,5 +4,36 @@ package at.devfest.fnddd.orders.domain
  * Order domain model boundary
  */
 object order {
+  /** Layering of imports */
+  import org.joda.time.DateTime
+  import at.devfest.fnddd.orders.domain.customer._
+  import at.devfest.fnddd.orders.domain.product._
+
+  import scala.collection.mutable.ArrayBuffer
+
+  /** Order ID domain value */
+  class OrderId(var id: String)
+
+  /** Address domain value */
+  class Address(
+    var street: String,
+    var zipCode: String,
+    var city: String,
+    var country: String)
+
+  /** Order line domain value */
+  class OrderLine(
+    var productId: ProductId,
+    var quantity: Int,
+    var unit: ProductUnit,
+    var price: BigDecimal )
+
+  /** Order domain entity, aggregate root */
+  class Order(
+    var id: OrderId,
+    var customerId: CustomerId,
+    var shipmentAddress: Address,
+    var lines: ArrayBuffer[OrderLine] = ArrayBuffer.empty,
+    var created: DateTime = DateTime.now())
 
 }
